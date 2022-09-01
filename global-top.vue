92c2b30453da655e88916db82475368b58b3f503
Konstantin BIFERT <hello@kissu.io>
2022-09-01 13:47:18 +0200

    correct url

--- global-top.vue
+++ global-top.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="absolute top-4 right-4 text-teal-500" v-if="$slidev.nav.currentPage > 2 && $slidev.nav.currentPage < 12">
-    <a href="https://w2d22.kissu.io" target="_blank">devc22.kissu.io</a>
+    <a href="https://devc22.kissu.io" target="_blank">devc22.kissu.io</a>
   </div>
 </template>
